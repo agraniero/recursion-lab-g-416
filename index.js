6d7de65b851e286b6d2edfc3147db4769654470d
Anthony Graniero <ananfro@gmail.com>
2020-09-03 21:08:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ function includesNumber(arr, n){
   } else if (arr[0] === n) {
     return true;
   } else { 
-    includesNumber(arr.slice(arr.length - 1), n);
+    return includesNumber(arr.slice(arr.length - 1), n);
   }
 }
